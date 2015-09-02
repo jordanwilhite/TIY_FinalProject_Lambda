f09fbd71532c02c69edd8ca6b1b17996da67c477
Jordan Wilhite <jordanwilhite@gmail.com>
2015-09-02 16:09:18 -0500

    React to NodeModules from BowerComponents, redid app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,14 @@
-var NavElement = React.createClass({
-  render: function(){
-    return (
-      
-    )
+import React from 'react';
+
+class NavElement extends React.createClass{
+    render(){
+      return (
+        <div class="Navigation">
+        <img src=""></img>
+        <h1>Lambda | &lambda;</h1>
+
+        </div>
+      )
+    }
   }
-});
-React.render(<NavElement />, document.getElementById('app'));
+React.render(<NavElement/> , document.getElementById('app'));
