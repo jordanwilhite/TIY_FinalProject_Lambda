3302dd56060eee55b5313741787c3fb65e183186
Jordan Wilhite <jordanwilhite@gmail.com>
2015-09-04 11:14:36 -0500

    search to btns on search and other changes

--- src/js/components/searchview.js
+++ src/js/components/searchview.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import Router from 'react-router';
+import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
 
 var SearchView = React.createClass({
   render: function(){
     return(
       <div>
         <button type="button">Tables</button>
-        <select>
-          <option>Mechanics</option>
-          <option>Waves</option>
-          <option>Electricity</option>
-          <option>Geometry & Trigonometry</option>
-        </select>
+        <button type="button">Mechanics</button>
+        <button type="button">Waves</button>
+        <button type="button">Electricity</button>
+        <button type="button">Geometry & Trigonometry</button>
       </div>
     )
   }
