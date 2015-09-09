7f25dcb255055daa63c60cfa67288104d5121bd0
Jordan Wilhite <jordanwilhite@gmail.com>
2015-09-09 16:17:14 -0500

    added jquery search element and removed searchview element.

--- src/js/components/searchview.js
+++ src/js/components/searchview.js
@@ -1,28 +1,28 @@
-import React from 'react';
-
-var SearchView = React.createClass({
-  render: function(){
-    return(
-      <div className="searchdrops">
-        <select className="table">
-            <option>TABLES</option>
-        </select>
-        <select className="mech">
-          <option>MECHANICS</option>
-        </select>
-        <select className="waves">
-          <option>WAVES</option>
-        </select>
-        <select className="elec">
-          <option>ELECTRICITY</option>
-        </select>
-        <select className="geotrig">
-          <option>GEOMETRY & TRIGONOMETRY</option>
-        </select>
-      </div>
-
-    )
-  }
-});
-
-export default SearchView;
+// import React from 'react';
+//
+// var SearchView = React.createClass({
+//   render: function(){
+//     return(
+//       <div className="searchdrops">
+//         <select className="table">
+//             <option>TABLES</option>
+//         </select>
+//         <select className="mech">
+//           <option>MECHANICS</option>
+//         </select>
+//         <select className="waves">
+//           <option>WAVES</option>
+//         </select>
+//         <select className="elec">
+//           <option>ELECTRICITY</option>
+//         </select>
+//         <select className="geotrig">
+//           <option>GEOMETRY & TRIGONOMETRY</option>
+//         </select>
+//       </div>
+//
+//     )
+//   }
+// });
+//
+// export default SearchView;
