c99de0ba76463cdfb1ed67a490b845b9255c2323
Jordan Wilhite <jordanwilhite@gmail.com>
2015-09-10 08:59:43 -0500

    changed ul to li's

--- src/js/components/geotrig.js
+++ src/js/components/geotrig.js
@@ -5,33 +5,33 @@ var GeotrigVariables = React.createClass({
     return(
       <div>
         <h1>Geometry &<br></br>Trigonometry Variables</h1>
-          <li nameClass="gtlist">
-            <ul>A = area</ul>
-            <ul>C = circumference</ul>
-            <ul>V = volume</ul>
-            <ul>S = surface area</ul>
-            <ul>b = base</ul>
-            <ul>h = height</ul>
-            <ul>l = length</ul>
-            <ul>w = width</ul>
-            <ul>r = radius</ul>
-          </li>
+          <ol nameClass="gtlist">
+            <li>A = area</li>
+            <li>C = circumference</li>
+            <li>V = volume</li>
+            <li>S = surface area</li>
+            <li>b = base</li>
+            <li>h = height</li>
+            <li>l = length</li>
+            <li>w = width</li>
+            <li>r = radius</li>
+          </ol>
         <h1>Formulas</h1>
-          <li nameClass="gtlist">
-            <ul>A = bh</ul>
-            <ul>A = 1/2bh</ul>
-            <ul>A = &pi;r<sup>2</sup></ul>
-            <ul>C = 2&pi;r</ul>
-            <ul>V = lwh</ul>
-            <ul>V = &pi;r<sub>2</sub>l</ul>
-            <ul>S = 2&pi;rl + &pi;r<sub>2</sub></ul>
-            <ul>V = 4/3&pi;r<sub>3</sub></ul>
-            <ul>S = 4&pi;r<sub>2</sub></ul>
-            <ul>c<sub>2</sub> = a<sub>2</sub> + b<sub>2</sub></ul>
-            <ul>sin&theta; = a/c</ul>
-            <ul>cos&theta; = b/c</ul>
-            <ul>tan&theta; = a/b</ul>
-          </li>
+          <ol nameClass="gtlist">
+            <li>A = bh</li>
+            <li>A = 1/2bh</li>
+            <li>A = &pi;r<sup>2</sup></li>
+            <li>C = 2&pi;r</li>
+            <li>V = lwh</li>
+            <li>V = &pi;r<sub>2</sub>l</li>
+            <li>S = 2&pi;rl + &pi;r<sub>2</sub></li>
+            <li>V = 4/3&pi;r<sub>3</sub></li>
+            <li>S = 4&pi;r<sub>2</sub></li>
+            <li>c<sub>2</sub> = a<sub>2</sub> + b<sub>2</sub></li>
+            <li>sin&theta; = a/c</li>
+            <li>cos&theta; = b/c</li>
+            <li>tan&theta; = a/b</li>
+          </ol>
       </div>
     )
   }
--- src/js/components/waves.js
+++ src/js/components/waves.js
@@ -2,21 +2,22 @@ import React from 'react';
 
 var WaveVariables = React.createClass({
   render: function (){
-    return(
-      <div>
-        <h1>Wave Variables</h1>
-          <li nameClass="wavelist">
-            <ul>f = frequency</ul>
-            <ul>v = speed</ul>
-            <ul>&lambda; = wavelength</ul>
-          </li>
-        <h1>Formula</h1>
-          <li>
-            <ul>&lambda; = v/f</ul>
-          </li>
+   return(
+       <div>
+         <h1>Wave Variables</h1>
+           <ol nameClass="wavelist">
+             <li><a href="frequency"></a>f = frequency</li>
+             <li>v = speed</li>
+             <li>&lambda; = wavelength</li>
+           </ol>
+         <h1>Formula</h1>
+           <ol>
+             <li>&lambda; = v/f</li>
+            </ol>
       </div>
     )
   }
 });
+
 
 export default WaveVariables
