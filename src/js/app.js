68a0087b8dee3b09e81997e7adfcbe6f1182fd8d
Jordan Wilhite <jordanwilhite@gmail.com>
2015-09-10 13:02:52 -0500

    added jquery search. It show's up but doesn't search yet

--- src/js/app.js
+++ src/js/app.js
@@ -19,6 +19,34 @@ let routes = (
   </Route>
 )
 
+$(document).ready(function() {
+    $(document).anysearch({
+        reactOnKeycodes: 'all',
+        secondsBetweenKeypress: 1,
+        searchPattern: {1: '[^~,]*'},
+        minimumChars: 3,
+        liveField: {selector: '#liveField', value: true},
+        excludeFocus: 'input,textarea,select,#tfield',
+        enterKey: 13,
+        backspaceKey: 8,
+        searchSlider: true,
+        startAnysearch: function() {
+            openHelp();
+        },
+        stopAnysearch: function() {
+            closeHelp();
+        },
+        minimumCharsNotReached: function(string, stringLength, minLength) {
+            alert(string + ' has ' + stringLength + ' chars! Minlength: ' + minLength);
+        },
+        searchFunc: function(string) {
+            doAjaxSearch(string);
+        },
+        patternsNotMatched: function(string, patterns) {
+            alert(string + ' must be in this form: ' + patterns);
+        },
+    });
+});
 
 Router.run(routes, function (Handler) {
 React.render(<Handler/>, document.getElementById('app'));
