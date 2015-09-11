7011008532aaba95dee0a9e168b6a052049cb67f
Jordan Wilhite <jordanwilhite@gmail.com>
2015-09-11 07:54:16 -0500

    Added Electricity and Magnetism Variables and Formulas.

--- src/js/components/electricity.js
+++ src/js/components/electricity.js
@@ -9,7 +9,7 @@ var ElectricVariables = React.createClass({
               <li>A = area</li>
               <li>F = force</li>
               <li>I = current</li>
-              <li>l = length</li>
+              <li>&#x2113; = length</li>
               <li>P = power</li>
               <li>q = charge</li>
               <li>R = resistance</li>
@@ -18,15 +18,71 @@ var ElectricVariables = React.createClass({
               <li>V = electric potential</li>
               <li>&rho; = resistivity</li>
             </ol>
-          <h1>Formulas</h1>
-          <ol>
+        <h1>Formulas</h1>
+          <ol nameClass="electriclist">
             <li>F<sub>E</sub> = k (q<sub>1</sub>q<sub>2</sub>/r<sup>2</sup>)</li>
             <li>I = &Delta;q/&Delta;t</li>
-            <li>R = &rho;l/A</li>
+            <li>R = &rho;&#x2113;/A</li>
             <li>I = &Delta;V/R</li>
             <li>P = I&Delta;V</li>
             <li>R<sub>s</sub> = &Sigma;<sub>i</sub>R<sub>i</sub></li>
             <li>1/R<sub>p</sub> = &Sigma;<sub>i</sub>1/R<sub>i</sub></li>
+          </ol>
+        <h1>Electricity & Magnetism Variables</h1>
+          <ol nameClass="electriclist">
+            <li>A = area</li>
+            <li>B = magnetic field</li>
+            <li>C = capacitance</li>
+            <li>d = distance</li>
+            <li>E = electric field</li>
+            <li>&Epsilon; = emf</li>
+            <li>F = force</li>
+            <li>I = current</li>
+            <li>&#x2113; = length</li>
+            <li>P = power</li>
+            <li>Q = charge</li>
+            <li>q = point charge</li>
+            <li>R = resistance</li>
+            <li>r = separation</li>
+            <li>t = time</li>
+            <li>U = potential (stored) energy</li>
+            <li>V = electric potential</li>
+            <li>&nu; = speed</li>
+            <li>&kappa; = dielectric constant</li>
+            <li>&rho; = resistivity</li>
+            <li>&theta; = angle</li>
+            <li>&Phi; = flux</li>
+          </ol>
+        <h1>Electricity & Magnetism Formulas</h1>
+          <ol nameClass="electriclist">
+            <li>|F<sub>E</sub>| = 1/4<sub>&pi;&epsilon;<sub>0</sub></sub>|q<sub>1</sub>q<sub>2</sub>|/r<sup>2</sup></li>
+            <li>E = F<sub>E</sub>/q</li>
+            <li>|E|  = 1/4<sub>&pi;&epsilon;<sub>0</sub></sub>|q<sub>2</sub>|/r<sup>2</sup></li>
+            <li>&Delta;U<sub>E</sub> = q&Delta;V</li>
+            <li>V = 1/4<sub>&pi;&epsilon;<sub>0</sub></sub>q/r</li>
+            <li>|E| = |&Delta;V/&Delta;r|</li>
+            <li>&Delta;V = Q/C</li>
+            <li>C = &kappa;&epsilon;<sub>0</sub>A/d</li>
+            <li>E = Q/&epsilon;<sub>0</sub>A</li>
+            <li>U<sub>c</sub> = 1/2Q&Delta;V = 1/2C(&Delta;V)<sup>2</sup></li>
+            <li>I = &Delta;Q/&Delta;t</li>
+            <li>R = &rho;l/A</li>
+            <li>P = I&Delta;V</li>
+            <li>I = &Delta;V/A</li>
+            <li>R<sub>s</sub> = &Sigma;<sub>i</sub>R<sub>i</sub></li>
+            <li>1/R<sub>p</sub> = &Sigma;<sub>i</sub>1/R<sub>i</sub></li>
+            <li>C<sub>p</sub> = &Sigma;<sub>i</sub>C<sub>i</sub></li>
+            <li>1/C<sub>s</sub> = &Sigma;<sub>i</sub>1/C<sub>i</sub></li>
+            <li>B = &mu;<sub>0</sub>I/2&pi;r</li>
+            <li>F<sub>m</sub> = q&nu; &times; B</li>
+            <li>|F<sub>M</sub>| = |q&nu;|sin&theta;||B|</li>
+            <li>F<sub>M</sub> = I&#x2113; &times; B</li>
+            <li>|F<sub>M</sub>| = |I&#x2113;||sin&theta;||B|</li>
+            <li>&Phi;<sub>B</sub> = B&#8226;A</li>
+            <li>&Phi;<sub>B</sub> = |B|cos&theta;|A|</li>
+            <li>&Epsilon; = -&Delta;&Phi;<sub>B</sub>/&Delta;t</li>
+            <li>&Epsilon; = B&#x2113;&nu;</li>
+            <li></li>
           </ol>
       </div>
     )
