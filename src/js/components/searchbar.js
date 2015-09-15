f2fd8fe527a9aa7bad43a65bceb21a31c8d35439
Jordan Wilhite <jordanwilhite@gmail.com>
2015-09-15 09:25:38 -0500

    made some changes

--- src/js/components/searchbar.js
+++ src/js/components/searchbar.js
@@ -3,26 +3,6 @@ import Parse from 'parse';
 
 export default class SearchBar extends React.Component {
 
-
-
-  search(e) {
-    if (!e.target.value) {
-    return searchValue.result([]);
-  }
-
-    let PhysicsForms = Parse.Object.extend("PhysicsForms"),
-        query = new Parse.Query(PhysicsForms);
-
-    query.startsWith("label", e.target.value);
-
-    query.ascending();
-    query.find({
-      success: (list) =>{
-        searchValue.result(list);
-      }
-    });
-  }
-
   render() {
     return(
       <div>
