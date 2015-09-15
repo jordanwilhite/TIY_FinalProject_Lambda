7a12944f07353d6fb3b1018dad0b4064fb410679
Jordan Wilhite <jordanwilhite@gmail.com>
2015-09-15 15:59:44 -0500

    updated CSS and logoview

--- src/js/components/logoview.js
+++ src/js/components/logoview.js
@@ -7,7 +7,7 @@ export default class LogoView extends React.Component {
         <img src={"../Lambda Logo/PNG/Lambda-Medium.png"}></img>
         <h1 className="maintitle">lambda</h1>
         <h2 className="subtitle">a physics aid | &lambda;</h2>
-        <h3 className="author"> Jordan Wilhite, 2015 | an iron yard final project</h3>
+        <h3 className="author"> jordan wilhite | an iron yard final project</h3>
       </div>
     )
   }
