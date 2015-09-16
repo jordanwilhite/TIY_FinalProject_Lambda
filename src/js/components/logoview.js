1016164c79f9f2755a147ceb16159654e1df0ad4
Jordan Wilhite <jordanwilhite@gmail.com>
2015-09-16 14:23:28 -0500

    new image src and styled search results

--- src/js/components/logoview.js
+++ src/js/components/logoview.js
@@ -4,7 +4,7 @@ export default class LogoView extends React.Component {
   render(){
     return(
       <div>
-        <img src={"../Lambda Logo/PNG/Lambda-Medium.png"}></img>
+        <img src={"css/Lambda-Medium.png"}></img>
         <h1 className="maintitle">lambda</h1>
         <h2 className="subtitle">a physics aid | &lambda;</h2>
         <h3 className="author"> jordan wilhite | an iron yard final project</h3>
